f73ff1e85aee52fee1c7aaa626e65b253d5edd13
Desmond Somtochukwu <dsomto98@gmail.com>
2025-05-28 05:13:58 +0100

    Updated Version 1.2

--- Blog/settings.py
+++ Blog/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load_dotenv 
+# Loads varibles from .env file
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
